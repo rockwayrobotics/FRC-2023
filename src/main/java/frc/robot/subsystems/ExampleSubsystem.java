58342a61e4dd5f1a7b71007b1fc0ef1807d5ec84
GqrlicBread <EKorczak@student.rockway.ca>
2023-01-13 16:20:37 -0500

    test commit

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -9,14 +9,15 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ExampleSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
   public ExampleSubsystem() {}
-
+//Eric was here?
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
-
+  
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
+
   }
 }
