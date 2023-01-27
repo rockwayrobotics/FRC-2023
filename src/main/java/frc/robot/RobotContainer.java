d5fb59bb6bb426edb1ce979526a1b1f429865970
Owen V <owenvandermeer@gmail.com>
2023-01-27 01:13:19 -0500

    Theoretically working drivebase code using separate DriveCommand, will test then merge into main.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,18 +4,15 @@
 
 package frc.robot;
 
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
 import frc.robot.Constants.CAN;
 import frc.robot.Constants.Controllers;
 import frc.robot.Constants.Digital;
-import frc.robot.subsystems.DrivebaseSubsystem;
 
-// import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
-// import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.Commands;
 
 public class RobotContainer {
 
@@ -28,20 +25,19 @@ public class RobotContainer {
 
   private XboxController m_xboxController = new XboxController(Controllers.XBOX);
 
+  SendableChooser<Command> m_chooser = new SendableChooser<>();
 
   public RobotContainer() {
     configureBindings();
+
+    m_drivebase.setDefaultCommand(new DriveCommand(() -> m_xboxController.getLeftX(), () -> m_xboxController.getLeftY(), m_drivebase));
   }
 
-    private void configureBindings() {    
-    m_drivebase.setDefaultCommand(
-      new RunCommand(
-        () -> m_drivebase.set(-m_xboxController.getLeftY(), m_xboxController.getLeftX()),
-        m_drivebase
-      )
-    );}
+  private void configureBindings() {    
+  }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    // The selected command will be run in autonomous
+    return m_chooser.getSelected();
   }
 }
