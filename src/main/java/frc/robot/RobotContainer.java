f3971f6c9ca3f065c0aafefb37ae53bf9df7b710
ovandermeer <owenvandermeer@gmail.com>
2023-01-28 14:32:33 -0500

    Mess of a commit bc I broke Git, but added theoretically working AlignRobotToTarget command and merged current state of main into this branch

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,3 +1,4 @@
+// ROBOTCONTAINER
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
@@ -7,13 +8,16 @@ package frc.robot;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 import frc.robot.Constants.*;
-
-import frc.robot.subsystems.CameraSubsystem;
-
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+import frc.robot.commands.DriveDistance;
 
 public class RobotContainer {
 
@@ -26,18 +30,52 @@ public class RobotContainer {
 
   private XboxController m_xboxController = new XboxController(Gamepads.XBOX);
 
+  private CameraSubsystem m_CameraSubsystem = new CameraSubsystem(m_xboxController);
+
+  private ShuffleboardTab m_autoSelectorTab = Shuffleboard.getTab("Auto Selection");
+
   SendableChooser<Command> m_chooser = new SendableChooser<>();
+
+  public final GenericEntry autoSpeed;
+
+  public final Command m_driveForever;
+  public final Command m_driveForeverSlow;
+
 
   public RobotContainer() {
     configureBindings();
+    var autoTab = Shuffleboard.getTab("Auto");
+    autoSpeed = autoTab.addPersistent("Max Speed", 1).getEntry();
+    
+    m_driveForever = new DriveDistance(m_drivebase, () -> autoSpeed.getDouble(0.5));
+    m_driveForeverSlow = new DriveDistance(m_drivebase, () -> 0.1);
+
+    m_chooser.setDefaultOption("Drive Forever", m_driveForever);
+    m_chooser.addOption("Drive Forever Slow", m_driveForeverSlow);
+    autoTab.add(m_chooser);
+
+    var auto1 = new SetDriveScaleCommand(m_drivebase, 0);
+    var auto2 = new SetDriveScaleCommand(m_drivebase, 0.5);
+    var auto3 = new SetDriveScaleCommand(m_drivebase, 0.7);
+
+    m_chooser.addOption("0 Speed", auto1);
+    m_chooser.addOption("0.5 Speed", auto2);
+    m_chooser.addOption("0.7 Speed", auto3);
+    m_autoSelectorTab.add(m_chooser);
 
     m_drivebase.setDefaultCommand(new DriveCommand(() -> m_xboxController.getLeftY(), () -> m_xboxController.getLeftX(), m_drivebase));
+
+    Shuffleboard.getTab("Subsystems").add(m_CameraSubsystem);
+    Shuffleboard.getTab("Subsystems").add(m_drivebase);
   }
 
   private void configureBindings() {
     final JoystickButton rightBumper = new JoystickButton(m_xboxController, XboxController.Button.kLeftBumper.value);
     rightBumper.onTrue(new SetDriveScaleCommand(m_drivebase, Drive.SLOMODE_SCALE));
     rightBumper.onFalse(new SetDriveScaleCommand(m_drivebase, 1));
+
+    final JoystickButton buttonA = new JoystickButton(m_xboxController, XboxController.Button.kA.value);
+    buttonA.onTrue(new AlignRobotToTarget(m_drivebase, m_CameraSubsystem));
   }
 
   public Command getAutonomousCommand() {
---
+++ src/main/java/frc/robot/commands/AlignRobotToTarget.java
@@ -0,0 +1,108 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.CameraSubsystem;
+import frc.robot.subsystems.DrivebaseSubsystem;
+
+import java.util.Map;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class AlignRobotToTarget extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final DrivebaseSubsystem m_DrivebaseSubsystem;
+  private final CameraSubsystem m_CameraSubsystem;
+
+  private PIDController turnController;
+
+  private SimpleWidget colorWidget;
+  private GenericEntry colorWidgetEntry;
+
+  private boolean foundTarget;
+  private int cyclesEmpty;
+  private boolean finishedRunning;
+
+  double rotationSpeed;
+
+  /**
+   * Creates a new AlignRobotToTarget.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public AlignRobotToTarget(DrivebaseSubsystem DrivebaseSubsystem, CameraSubsystem CameraSubsystem) {
+    m_DrivebaseSubsystem = DrivebaseSubsystem;
+    m_CameraSubsystem = CameraSubsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(DrivebaseSubsystem);
+    addRequirements(CameraSubsystem);
+
+    final double ANGULAR_P = 0.1;
+    final double ANGULAR_D = 0.0;
+
+    turnController = new PIDController(ANGULAR_P, 0, ANGULAR_D);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    foundTarget = false;
+    cyclesEmpty = 0;
+    rotationSpeed = 0;
+    
+    finishedRunning = false;
+
+    // Create Boolean widget that displays the color
+    colorWidget = Shuffleboard.getTab("Vision").add("LostTarget", false);
+    colorWidget.withPosition(0, 4);
+    colorWidget.withProperties(Map.of("colorWhenFalse", "black"));
+    colorWidget.withProperties(Map.of("colorWhenTrue", "red"));
+    colorWidgetEntry = colorWidget.getEntry();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    
+    var result = m_CameraSubsystem.camera.getLatestResult();
+
+    if(result.hasTargets()) {
+        foundTarget = true;
+        
+        rotationSpeed = -turnController.calculate(result.getBestTarget().getYaw(), 0);
+
+        m_DrivebaseSubsystem.set(0, rotationSpeed);
+    } else {
+      if(foundTarget) {
+        m_DrivebaseSubsystem.set(0, rotationSpeed);
+        cyclesEmpty++;
+
+        if(cyclesEmpty >= 100) {
+            colorWidgetEntry.setBoolean(true);
+            finishedRunning = true;
+        }
+      } else {
+        finishedRunning = true;
+      }
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return finishedRunning;
+  }
+}
