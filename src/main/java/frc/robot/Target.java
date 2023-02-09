0ab78bb9f68d82f5be84ba5f61c4ac47a979a242
23nicsat <23nicsat@student.rockway.ca>
2023-02-09 16:00:19 -0500

    added support for different piston powers

--- src/main/java/frc/robot/Target.java
+++ src/main/java/frc/robot/Target.java
@@ -1,16 +1,43 @@
 package frc.robot;
 
-
 /**
  * Class that holds information and methods about Nodes
  */
 public class Target {
+
+    /**
+     * Holds the choice between a Cube Node and Cone Node
+     */
+    enum TargetType {
+        Cube,
+        Cone;
+    }
+
+    /**
+     * Holds the choice between taking the higher angle (come in with more y velocity and less x velocity) or take the lower angle (more x velocity and less y velocity)
+     */
+    enum Approach {
+        High,
+        Low;
+    }
+
+    /**
+     * Holds the choice between shooting with high or low amounts of power
+     */
+    enum PistonPower {
+        High,
+        Low;
+    }
+
     public double offset;
     public double height;
-    public Target(double h, double o){
-        // height = height;
-        // offset = offset;
+    public TargetType type;
+    public Approach approach_type;
+    
+    public Target(double h, double o, TargetType t, Approach a){
+        type = t;
         height = h;
         offset = o;
+        approach_type = a;
     }
 }
